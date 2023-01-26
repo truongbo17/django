b5c0c8082666dfa69ea50044adb6be19ec62f752
Nguyen Quang Truong <truongnq017@gmail.com>
2023-01-26 20:37:09 +0700

    paginate

--- members/views.py
+++ members/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import PostForm
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
     post_form = PostForm.objects.all()
-    return render(request, 'members/index.html', {'post_form': post_form})
+    paginator = Paginator(post_form, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'members/index.html', {'post_form': page_obj})
 
 
 def detail(request, id):
